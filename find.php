7bb45c914e5141cb01123a0de06708037d74ea2d
leostrupeni <leonardo.strupeni@gmail.com>
2019-06-06 15:46:42 -0300

    Maquetacion

---
+++ find.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,user-scalable=no">
+        <title>CSS Rejilla</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+      <div class="contenedor clearfix">
+        <div class="fila">
+            <div class="logo col-2"><img src="images/Drinks-42.svg" alt="img"></div>
+            <div class="navegacion col-8">
+              <div class="fila-media col-12"></div>
+              <div class="fila-media col-3"></div>
+              <div class="fila-media col-3"></div>
+              <div class="fila-media col-3"></div>
+              <div class="fila-media col-3"></div>
+            </div>
+            <div class="navegacion col-2">
+              <div class="fila-media col-12"></div>
+              <div class="fila-media col-12"></div>
+            </div>
+        </div>
+        
+        <div class="main">
+          <div class="col-3">
+            <div class="col-12 filtros"></div>
+          </div>
+
+          <div class="col-9">
+            <div class="body-search">
+                <div class="col-4"></div>
+                <div class="col-4"></div>
+                <div class="col-4"></div>
+            </div>
+            <div class="body-search">
+                <div class="col-4"></div>
+                <div class="col-4"></div>
+                <div class="col-4"></div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-12">
+            <div class="col-12 footer"></div>
+            <div class="col-12 footer"></div>
+        </div>
+      </div>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,user-scalable=no">
+        <title>CSS Rejilla</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+      <div class="contenedor clearfix">
+        <div class="fila">
+            <div class="logo col-2"><img src="images/Drinks-42.svg" alt="img"></div>
+            <div class="navegacion col-8">
+              <div class="fila-media col-12"></div>
+              <div class="fila-media col-3"></div>
+              <div class="fila-media col-3"></div>
+              <div class="fila-media col-3"></div>
+              <div class="fila-media col-3"></div>
+            </div>
+            <div class="navegacion col-2">
+              <div class="fila-media col-12"></div>
+              <div class="fila-media col-12"></div>
+            </div>
+        </div>
+        
+        <div class="main">
+          <div class="col-12 contenido"></div>
+          <div class="col-12 body-pri"></div>
+        </div>
+        
+        <div class="col-12">
+            <div class="col-12 footer"></div>
+            <div class="col-12 footer"></div>
+        </div>
+      </div>
+  </body>
+</html>
