02f5963e3b4be1a7d240da9e0bda4b3e4e8b0166
leo Strupeni <leonardo.strupeni@gmail.com>
2019-06-09 21:46:15 -0300

    Estilos y Formulario Contacto y FAQ

--- Faq.php
+++ Faq.php
@@ -1,46 +1,338 @@
+<!DOCTYPE html>
+<html>
+<?php include ("head.php") ?>
+<body>
 
-<div class="accordion" id="accordionExample">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h2 class="mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </button>
-      </h2>
+  <div class="row">
+    <div class="col-12 text-right">
+      <div class="col-8">
+        <h2 class="text-light">Sobre tu cuenta</h2>
+      </div>
+      <div class="col-4">
+        <a href="index.php">Volver a Pagina Inicio</a>  
+      </div>
     </div>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+    <div class="col-12">
+
+      <div class="accordion" id="accordionExample">
+    
+        <div class="card">
+          <div class="card-header" id="headingOne">
+            <h3 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                ¿Cómo creo una cuenta?
+              </button>
+            </h3>
+          </div>
+
+          <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+              Haz clic en botón de Registro que se encuentra en la pate superior de la pantalla.
+              Completa el formulario de registro.
+              Luego vas a recibir un mail con la confirmación.
+              Listo ya estas registrado
+            </div>
+          </div>
+        </div>
+      
+        <div class="card">
+          <div class="card-header" id="headingTwo">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Tengo problemas para acceder a mi cuenta. ¿Qué hago?
+              </button>
+            </h2>
+          </div>
+          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+              Hay varias razones por las que puedes tener problemas al iniciar sesión. 
+        
+              Veo un mensaje de "información de inicio de sesión incorrecta. Vuelve a intentarlo"	
+                1. Recuerde que la contraseña es sensible a mayúsculas. Asegúrate de que la tecla Bloq Mayús está desactivada
+                2. Crea una nueva Contraseña.
+                3. Si sigues teniendo problemas, borra todas las contraseñas guardadas automáticamente. Esto garantizará que tu computadora o tu navegador no introduzcan de forma automática unas credenciales no válidas.
+              
+              Veo un mensaje de “no se pudo crear una cuenta nueva con esta información. Si ya tienes una cuenta, inicia sesión”.
+                Si recibes este error, significa que estás usando la página de creación de una cuenta en vez de la página de iniciar sesión.
+              
+              Olvidé mi contraseña
+              
+              Seleccione loguearse y luego ¿Olvidaste tu contraseña? 
+              Ingrese la dirección de correo electrónico asociada a tu cuenta.
+              Un correo electrónico será enviado a su bandeja de entrada - asegúrese de comprobar su carpeta de correo no deseado y las "Promociones" y pestañas 'sociales', si utiliza Gmail.
+              Haga clic en el enlace del correo electrónico. Cuando lo hayas hecho, por favor siga las instrucciones para crear una nueva contraseña.
+
+              Recuerde que la contraseña es sensible a mayúsculas, por lo que si  utilizas letras en mayúscula, debes utilizar letras mayúsculas al iniciar sesión en futuras ocasiones. 
+              Además, recuerda, no dar ninguna información personal, como tu contraseña.
+            </div>
+          </div>
+        </div>
+      
+        <div class="card">
+          <div class="card-header" id="headingThree">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Quiero recibir ofertas y recomendaciones de productos ¿Qué es lo que tengo que hacer?
+              </button>
+            </h2>
+          </div>
+          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div class="card-body">
+              Si querés recibir información y promociones de forma exclusiva no dudes en inscribirte en nuestro Newsletter! 
+              El envío de emails promocionales será realizado solo con tu consentimiento y podrás desactivar esta opción en cualquier momento.
+            </div>
+          </div>
+        </div>
+
+       </div>
+    </div>
+
+    <div class="col-12">
+      <h2 class="text-light">Compras</h2>
+    </div>
+
+    <div class="col-12">
+      <div class="accordion" id="accordionExample">
+     
+        <div class="card">
+          <div class="card-header" id="headingfour">
+            <h3 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapsefour" aria-expanded="true" aria-controls="collapsefour">
+                ¿Tengo que registrarme para realizar una compra? 
+              </button>
+            </h3>
+          </div>
+
+          <div id="collapsefour" class="collapse" aria-labelledby="headingfour" data-parent="#accordionExample">
+            <div class="card-body">
+              Sí, para realizar una compra es necesario que estés registrado. Podés hacerlo una vez que ya hayas llenado tu carrito!
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header" id="headingfive">
+            <h3 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapsefive" aria-expanded="true" aria-controls="collapsefive">
+                ¿Cómo efectúo una compra?
+              </button>
+            </h3>
+          </div>
+
+          <div id="collapsefive" class="collapse" aria-labelledby="headingfive" data-parent="#accordionExample">
+            <div class="card-body">
+                Paso 1: Para cargar los productos en tu carrito hacé click en el botón que dice “Comprar” , que aparece debajo de cada uno. 
+                    Para eliminar productos de tu carrito, podes hacerlo clickeando en la “X” que figura al lado del detalle de compra.
+                    Para cambiar las cantidades del producto elegido, hacelo directamente desde el carrito de compras, clickeando en “+” o “-” según lo que desees. 
+                    Una vez que hayas seleccionado todos los que desees, clickeá en “Finalizar compra” para avanzar en tu pedido. 
+
+                Paso 2: Seleccionas el domicilio de entrega 
+
+                Paso 3: Realizas el Pago
+                    Clickeando en “Ir a Pagar” vas a ser re-direccionado a la página de Mercado Pago para realizar de forma segura el pago.
+
+                Paso 4: Confirmación de compra 
+                    Vas a estar recibiendo un mail con la confirmación de tu compra y datos para el seguimiento de tu pedido. 
+
+                Paso 5: Confirmación de despacho 
+                    Una vez que tu compra sea despachada, vas a recibir un mail con el número de seguimiento. 
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header" id="headingsix">
+            <h3 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapsesix" aria-expanded="true" aria-controls="collapsesix">
+                ¿Hay un mínimo o maximo de productos para comprar?
+              </button>
+            </h3>
+          </div>
+
+          <div id="collapsesix" class="collapse" aria-labelledby="headingsix" data-parent="#accordionExample">
+            <div class="card-body">
+            No, podés elegir la cantidad que quieras y combinarlos como prefieras! 
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header" id="headingSeven">
+            <h3 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="true" aria-controls="collapseSeven">
+              ¿Puedo modificar mi compra una vez finalizada?
+              </button>
+            </h3>
+          </div>
+
+          <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordionExample">
+            <div class="card-body">
+              No, una vez que tu orden está terminada ya no es posible sumarle más productos. 
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header" id="headingEight">
+            <h3 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseEight" aria-expanded="true" aria-controls="collapseEight">
+              ¿Puedo cambiar el domicilio de entrega?
+              </button>
+            </h3>
+          </div>
+
+          <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordionExample">
+            <div class="card-body">
+            No es posible cambiar la dirección una vez finalizada la compra.
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header" id="headingNine">
+            <h3 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="true" aria-controls="collapseNine">
+              ¿Qué opciones tengo para pagar?
+              </button>
+            </h3>
+          </div>
+
+          <div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordionExample">
+            <div class="card-body">
+            Vos elegís con qué medio de pago hacerlo, dentro de las opciones que ofrezca la empresa proveedora del servicio.
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header" id="headingTen">
+            <h3 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseTen" aria-expanded="true" aria-controls="collapseTen">
+              ¿Qué pasa si no puedo ir a retirar el pedido?
+              </button>
+            </h3>
+          </div>
+
+          <div id="collapseTen" class="collapse" aria-labelledby="headingTen" data-parent="#accordionExample">
+            <div class="card-body">
+            En el caso de que sea otra la persona que retire tu compra, deberá contar con una autorización del titular y fotocopia del DNI del titular. ¿Qué debe decir la autorización?
+              - Nombre y Apellido del titular
+              - DNI del titular
+              - Nombre y Apellido del autorizado
+              - DNI del autorizado
+              - Número/s de envío que autoriza a retirar
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Collapsible Group Item #2
-        </button>
-      </h2>
+
+    <div class="col-12">
+      <h2 class="text-light">Envío y retiro de productos</h2>
     </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
+    <div class="col-12">
+
+      <div class="accordion" id="accordionExample">
+    
+        <div class="card">
+          <div class="card-header" id="headingEleven">
+            <h3 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseEleven" aria-expanded="true" aria-controls="collapseEleven">
+              ¿Cuál es el plazo, lugar y horario que puedo retirar mi compra?
+              </button>
+            </h3>
+          </div>
+
+          <div id="collapseEleven" class="collapse" aria-labelledby="headingEleven" data-parent="#accordionExample">
+            <div class="card-body">
+              En al seccion de domicilio de entrega usted podra configurar todos los detalles necesarios para recibir su pedido
+            </div>
+          </div>
+        </div>
+      
+        <div class="card">
+          <div class="card-header" id="headingTwelve">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwelve" aria-expanded="false" aria-controls="collapseTwelve">
+                ¿Cómo coordino el envío de mi producto?
+              </button>
+            </h2>
+          </div>
+          <div id="collapseTwelve" class="collapse" aria-labelledby="headingTwelve" data-parent="#accordionExample">
+            <div class="card-body">
+              Una vez confirmado el pago, el vendedor se contactara con usted para confirmar los datos de envio de los productos.
+            </div>
+          </div>
+        </div>
+      
+        <div class="card">
+          <div class="card-header" id="headingThirteen">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThirteen" aria-expanded="false" aria-controls="collapseThirteen">
+                Mis productos no me llegaron. ¿Que hago?
+              </button>
+            </h2>
+          </div>
+          <div id="collapseThirteen" class="collapse" aria-labelledby="headingThirteen" data-parent="#accordionExample">
+            <div class="card-body">
+              Si usted realizo el pago y sus productos no le llegan se le reintegrara a su cuenta el saldo.
+		          Tenga en cuenta que el pago no se libera hasta que usted no reciba el producto y confirme su recepcion.
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header" id="headingFourteen">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFourteen" aria-expanded="false" aria-controls="collapseFourteen">
+              Me llegaron mi productos, pero no confirme recepcion. 
+              </button>
+            </h2>
+          </div>
+          <div id="collapseFourteen" class="collapse" aria-labelledby="headingFourteen" data-parent="#accordionExample">
+            <div class="card-body">
+              Si no pudo confirmar la rececpcion de los productos y no realizo un reclamo sobre la compra. 
+		          Al vendedor se le acreditara el pago 24hs luego de la fecha en la que este confirmo haberlo entregado. 
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Collapsible Group Item #3
-        </button>
-      </h2>
+
+    <div class="col-12">
+      <h2 class="text-light">Atención al cliente</h2>
     </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
+    <div class="col-12">
+
+      <div class="accordion" id="accordionExample">
+
+        <div class="card">
+          <div class="card-header" id="headingFiveteen">
+            <h3 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseFiveteen" aria-expanded="true" aria-controls="collapseFiveteen">
+                Contactenos
+              </button>
+            </h3>
+          </div>
+
+          <div id="collapseFiveteen" class="collapse" aria-labelledby="headingFiveteen" data-parent="#accordionExample">
+            <div class="card-body">
+            Si querés contactarnos completa el formulario de contacto, estamos para vos!
+            </div>
+          </div>
+        </div>
+      
       </div>
     </div>
+
   </div>
-</div>
+ 
+
+<?php include ("Script-B.php") ?> 
+
+</body>
+
+</html>
--- footer.php
+++ footer.php
@@ -1,26 +1,34 @@
-<div class="row">
-    <div class="col-12">
-
+<div class="row text-center">
+    <div class="col-12 ">
+        <section class="mailing-list-section">
+            <h3 class="heading">NEWSLETTER</h3>
+            <p class="message">Dejanos tu mail para recibir promociones</p>
+            <form action="" method="post" name="subscribe-form" class="">
+                <div class="input-wrapper">
+                    <input type="email" class="mailchimp-input" value="" name="email" placeholder="Ingresá tu mail">
+                </div>
+            </form>
+        </section>
     </div>
-    <div class="col-12">
-        <div class="card text-center">
-            <div class="card-header">
-                <ul class="nav nav-tabs card-header-tabs">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Contacto</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Preguntas Frecuentes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Politicas de privacidad</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Terminos y Condiciones</a>
-                </li>
-                </ul>
-            </div>
+
+    <div class="card">
+        <div class="card-header text-center">
+            <ul class="nav nav-tabs card-header-tabs">
+            <li class="nav-item">
+                <a class="nav-link active" href="Contacto.php">Contacto</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="Faq.php">Preguntas Frecuentes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Politicas de privacidad</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Terminos y Condiciones</a>
+            </li>
+            </ul>
         </div>
     </div>
 </div>
+
 
