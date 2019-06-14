23ead777a50335f0ee1f287564b6ff04a084591a
leo Strupeni <leonardo.strupeni@gmail.com>
2019-06-13 23:22:54 -0300

    Agregado icono carrito y solucionado carousel

--- html/Menu-Nav.php
+++ html/Menu-Nav.php
@@ -1,87 +1,93 @@
 <div class="row">
-      <div class="col-6 col-md-2">
-         <img src="images/Drinks-42.svg" alt="logo" class="logo">
-      </div>
+  <div class="col-6 col-md-2">
+      <img src="images/Drinks-42.svg" alt="logo" class="logo">
+  </div>
 
-      <div class="col-6 col-md-2 p-0">
-        <a href="login.php" class="btn btn-success login">Login</a>
-        <a href="register.php" class="btn btn-success login">Registrate</a>
-        <a href="perfil.php" class="btn btn-success login">Perfil</a>
-      </div>
+  <div class="col-6 col-md-2 p-0">
+    <a href="login.php" class="btn btn-success login">Login</a>
+    <a href="register.php" class="btn btn-success login">Registrate</a>
+    <a href="perfil.php" class="btn btn-success login">Perfil</a>
+  </div>
 
-      <div class="col-12 col-md-8">
-        <div class="col-12 m-3">
-          <form class="form-inline my-2 my-lg-2">
-            <div class="col-10">
-              <input class="form-control" type="search" placeholder="Busqueda" aria-label="Search">
-            </div>
-            <div class="col-2">
-              <button class="btn btn-success my-2 my-sm-0" type="submit">
-                <i class="fas fa-search"></i>
-              </button>
-            </div>
-          </form>
+  <div class="col-12 col-md-8">
+  
+    <div class="col-12 m-3">
+      <form class="form-inline my-2 my-lg-2">
+        <div class="col-10">
+          <input class="form-control" type="search" placeholder="Busqueda" aria-label="Search">
         </div>
-
-        <div class="col-12 col-md-8">
-          <nav class="navbar navbar-expand-md navbar-light bg-transparent">
-
-            <button class="navbar-toggler btn bg-dark btn-lg btn-block" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="text-light dropdown-toggle">Menu</span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <strong>Cervezas</strong>
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <strong>Vinos</strong>
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <strong>Blancas</strong>
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <strong>Aperitivos</strong>
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </nav>
+        <div class="col-2">
+          <button class="btn btn-success my-2 my-sm-0" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
         </div>
-
-      </div>
-
+      </form>
     </div>
+
+  <div class="row">
+    <div class="col-10 col-md-9">
+      <nav class="navbar navbar-expand-md navbar-light bg-transparent">
+
+        <button class="navbar-toggler btn bg-dark btn-lg btn-block" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="text-light dropdown-toggle">Menu</span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <strong>Cervezas</strong>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <strong>Vinos</strong>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <strong>Blancas</strong>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <strong>Aperitivos</strong>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
+    <div class="col-2 col-md-2">
+      <button class="btn btn-danger btn-lg login" type="submit">
+        <i class="fas fa-store"></i>
+      </button>
+    </div>
+  </div>
+</div>
