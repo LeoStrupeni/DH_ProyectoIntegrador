44a7a39807d82ea22af1e972c9b1190f741431f5
leo Strupeni <leonardo.strupeni@gmail.com>
2019-08-15 21:24:38 -0300

    Laravel: Creacion Plantilla e Index

---
+++ resources/views/modals/modal-login.blade.php
@@ -0,0 +1,42 @@
+<button type="button" class="btn btn-nav btn-user" data-toggle="modal" data-target="#modalLogin">
+  Ingresar
+</button>
+
+<div class="modal fade" id="modalLogin" tabindex="-1" role="dialog" aria-labelledby="modalLogin" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+
+      <form action="index.php" class="form p-2" method="post">
+        <div class="modal-body">
+
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+
+          <div class="form-group">
+            <label for="email" class="h4">Email</label>
+            <input type="email" name="email" id="email" class="form-control">
+          </div>
+
+          <div class="form-group">
+            <label for="password" class="h4">Contraseña</label>
+            <input type="password" name="password" id="password" class="form-control">
+          </div>
+
+          <div>
+            <input type="checkbox" name="Recordar" value="recuerdo" class="form-check-input" checked>
+            <label class="form-check-label">
+              Recordarme
+            </label>
+          </div>
+
+        </div>
+
+        <div class="modal-footer justify-content-center">
+          <button type="submit" class="btn btn-modal">Ingresar</button>
+        </div>
+      </form>
+
+    </div>
+  </div>
+</div>
