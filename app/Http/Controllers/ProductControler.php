4e599681f095ca4faf15ed73c0b2fba686f0ca7e
leo Strupeni <leonardo.strupeni@gmail.com>
2019-08-23 13:43:29 -0300

    agregado pagina busqueda productos

---
+++ app/Http/Controllers/ProductControler.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductControler extends Controller
+{
+    public function search(){
+        $par=$_POST['ParamBusqueda'];
+        $products = Product::leftJoin('prod_categorias', 'Categoria', '=', 'prod_categorias.ID')
+                    ->leftJoin('prod_marcas', 'Marcas_idMarcas', '=', 'prod_marcas.ID')
+                    ->where('name','LIKE','%'.$par.'%')
+                    ->orwhere('Descripcion','LIKE','%'.$par.'%')
+                    ->orwhere('prod_categorias.Nombre','LIKE','%'.$par.'%')
+                    ->orwhere('prod_marcas.Nombre','LIKE','%'.$par.'%')
+                    ->paginate(50);
+
+        $vac = compact("products");
+        return view('/Search',$vac);
+    }
+}
