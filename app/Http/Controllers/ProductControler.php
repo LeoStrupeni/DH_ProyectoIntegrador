8a177754dd733f73866c7294c0aebf69dea59989
Santiago <sanfctz@gmail.com>
2019-08-24 14:43:54 -0300

    Arreglos de formato

--- app/Http/Controllers/ProductControler.php
+++ app/Http/Controllers/ProductControler.php
@@ -8,17 +8,18 @@ use App\Product;
 class ProductControler extends Controller
 {
     //Hay que migrar esto al ProductsController y cambiar los parametros por los de la nueva DB
-    public function search(){
-        $par=$_POST['ParamBusqueda'];
+    public function search()
+    {
+        $par = $_POST['ParamBusqueda'];
         $products = Product::leftJoin('prod_categorias', 'Categoria', '=', 'prod_categorias.ID')
-                    ->leftJoin('prod_marcas', 'Marcas_idMarcas', '=', 'prod_marcas.ID')
-                    ->where('name','LIKE','%'.$par.'%')
-                    ->orwhere('Descripcion','LIKE','%'.$par.'%')
-                    ->orwhere('prod_categorias.Nombre','LIKE','%'.$par.'%')
-                    ->orwhere('prod_marcas.Nombre','LIKE','%'.$par.'%')
-                    ->paginate(50);
+            ->leftJoin('prod_marcas', 'Marcas_idMarcas', '=', 'prod_marcas.ID')
+            ->where('name', 'LIKE', '%' . $par . '%')
+            ->orwhere('Descripcion', 'LIKE', '%' . $par . '%')
+            ->orwhere('prod_categorias.Nombre', 'LIKE', '%' . $par . '%')
+            ->orwhere('prod_marcas.Nombre', 'LIKE', '%' . $par . '%')
+            ->paginate(50);
 
         $vac = compact("products");
-        return view('/Search',$vac);
+        return view('/Search', $vac);
     }
 }
--- app/Sale.php
+++ app/Sale.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     public $guarded = [];
-    
+
     public function user()
     {
         return $this->belongsTo('App\User', 'user_id');
