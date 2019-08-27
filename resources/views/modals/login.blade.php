1eee177d0f419774a0c218411e8042b376a043d5
Santiago <sanfctz@gmail.com>
2019-08-27 00:57:25 -0300

    Arreglos visuales

--- resources/views/modals/login.blade.php
+++ resources/views/modals/login.blade.php
@@ -54,7 +54,7 @@
                                 <input class="form-check-input" type="checkbox" name="remember" id="remember"
                                     {{ old('remember') ? 'checked' : '' }}>
 
-                                <label class="form-check-label" for="remember">
+                                <label class="form-check-label align-middle" for="remember">
                                     {{ __('Recordarme') }}
                                 </label>
                             </div>
@@ -63,12 +63,12 @@
 
                     <div class="form-group row mb-0">
                         <div class="col-md-8 offset-md-3">
-                            <button type="submit" class="btn btn-primary">
+                            <button type="submit" class="btn btn-modal mr-1">
                                 {{ __('Ingresar') }}
                             </button>
 
                             @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                            <a class="btn btn-warning ml-1" href="{{ route('password.request') }}">
                                 {{ __('Olvidaste tu contraseña?') }}
                             </a>
                             @endif
