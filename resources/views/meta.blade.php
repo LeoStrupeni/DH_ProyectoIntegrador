18b0c75bd61a8b934388772d71f42b81b527797f
Santiago <sanfctz@gmail.com>
2019-09-07 19:23:26 -0300

    Implemento JQuery para modal restablecer password

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -15,6 +15,7 @@
     <script src="{{ URL::asset('js/pushbar.js') }}"></script>
     <script src="{{ URL::asset('js/search.js') }}"></script>
     <script src="{{ URL::asset('js/custom.js') }}"></script>
+    <script src="{{ URL::asset('js/modals.js') }}"></script>
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
         integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
--- resources/views/modals/login.blade.php
+++ resources/views/modals/login.blade.php
@@ -71,15 +71,48 @@
                             </button>
 
                             @if (Route::has('password.request'))
-                            <a class="btn btn-warning ml-1" href="{{ route('password.request') }}">
+                            <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalReset"
+                                id="modalResetBtn">
                                 {{ __('Olvidaste tu contraseña?') }}
-                            </a>
+                            </button>
                             @endif
                         </div>
                     </div>
                 </div>
             </form>
 
+            <form action="{{ route('password.email') }}" class="form" method="POST" id="formReset">
+                @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+                @endif
+
+                <div class="form-group row">
+                    <label for="email"
+                        class="col-md-4 col-form-label text-md-right font-weight-bold">{{ __('Correo electronico') }}</label>
+
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+
+                <div class="form-group row">
+                    <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Enviar link para restablecer password') }}
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
